1fcd501a6f650640de3b6ff131ba9d0e68e98007
Your NamePratham <pra172005@gmail.com>
2025-02-23 04:42:32 +0530

    final commit

--- Project/backend/server.js
+++ Project/backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const multer = require('multer');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,10 +10,28 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Sample route
-app.get("/", (req, res) => {
-    res.json({ message: "Hello, World!" });
-});
+// // Sample route
+// app.get("/", (req, res) => {
+//     res.json({ message: "Hello, World!" });
+// });
+
+
+const storage = multer.diskStorage({
+  destination : function(req, file, cb) {
+    return cb(null, "./public/")
+  },
+  filename: function(req, file, cb) {
+    return cb(null, `${Date.now()}_${file.originalname}`)
+  }
+})
+
+const uplaod = multer({storage})
+
+app.post('/upload', uplaod.single('file'), (req,res) =>{
+    console.log(req.body)
+    console.log(req.file)
+})
+
 
 // Start server
 app.listen(PORT, () => {
