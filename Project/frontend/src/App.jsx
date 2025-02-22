6e0dbe1ae75ff4ccc4a3ddde23141c4a3da3027b
Your NamePratham <pra172005@gmail.com>
2025-02-23 04:44:20 +0530

    final push

--- Project/frontend/src/App.jsx
+++ Project/frontend/src/App.jsx
@@ -1,11 +1,24 @@
-
+import React, { useState } from "react";
+import axios from 'axios';
 
 function App() {
 
-  return (
+  const [file, setFile] = useState();
 
+  const upload = () => {
+
+    const FormData = new FormData()
+    FormData.append('file', file)
+
+    axios.post('http://localhost:5000/upload', formData)
+    .then(res => {})
+    .catch(er => console.log(er))
+  }
+
+  return (
       <div>
-        <h1>Frontend</h1>
+       <input type="file" onChange={() => setFile(e.target.files[0])} />  
+      <button type="button" onClick={upload}>Upload</button>
       </div>
 
   )
